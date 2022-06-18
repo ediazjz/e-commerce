1f9792d90310576b27237aa987f9d490946a1157
ediazjz <iam.ediazjz@gmail.com>
2022-06-18 12:43:54 -0500

    feat: add Cloudinary domain and localization options

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["res.cloudinary.com", "avatars.dicebear.com"],
+  },
+  i18n: {
+    locales: ["es", "en"],
+    defaultLocale: "es",
+  },
 }
 
 module.exports = nextConfig
