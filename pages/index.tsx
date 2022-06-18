e69d490bc85b8c83d24e0f780523ee70a4d861bc
ediazjz <iam.ediazjz@gmail.com>
2022-06-18 11:17:03 -0500

    dependency: install Prettier and its Tailwind integration

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,9 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next"
 
 const Home: NextPage = () => {
   return (
-    <main className='justify-center w-screen h-screen bg-blue-400 flex items-center'>
-      <h1 className='text-white'>
-        Hello, e-commerce!
-      </h1>
+    <main className="flex h-screen w-screen items-center justify-center bg-blue-400">
+      <h1 className="text-white">Hello, e-commerce!</h1>
     </main>
   )
 }
