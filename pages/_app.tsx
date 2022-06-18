5d86e863945dbd711c168384512e39323d2efd50
ediazjz <iam.ediazjz@gmail.com>
2022-06-18 11:32:20 -0500

    dependency: install URQL

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app"
+
+import { createClient, Provider } from "urql"
+
+import "../styles/globals.css"
+
+const client = createClient({
+  url: process.env.NEXT_PUBLIC_BACKEND_GRAPHQL_ENDPOINT!,
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Provider value={client}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
 
 export default MyApp
