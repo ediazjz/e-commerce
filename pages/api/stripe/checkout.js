5d837f481dedeef9701f09d87374537831cb44f5
ediazjz <iam.ediazjz@gmail.com>
2022-06-22 21:32:15 -0500

    feat: get Auth0 session and fetch user Stripe id

--- pages/api/stripe/checkout.js
+++ pages/api/stripe/checkout.js
@@ -1,13 +1,20 @@
 import Stripe from "stripe"
+import { getSession } from "@auth0/nextjs-auth0"
 
 const stripe = new Stripe(`${process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY}`)
 
 export default async function processCheckout(req, res) {
+  const session = getSession(req, res)
+  const stripeId = session?.user[`${process.env.BASE_URL}/stripe_customer_id`]
+
+  console.log(user)
+
   if (req.method === "POST") {
     try {
       const session = await stripe.checkout.sessions.create({
         submit_type: "pay",
         mode: "payment",
+        customer: stripeId,
         payment_method_types: ["card"],
         shipping_address_collection: {
           allowed_countries: ["MX"],
